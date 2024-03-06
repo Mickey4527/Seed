70e4e34de9886f68e2e5b6f6d7c9ca9752ed005c
Mickey4527 <saran.za097@gmail.com>
2024-03-06 20:10:15 +0700

    Fix bug in login functionality

---
+++ front-end/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-gray-600">404 - Page Not Found</h1>
+        </div>
+    );
+}
